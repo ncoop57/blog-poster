1b19cd2d26317be10ba24580e9d57fb37e2398cd
Nathan Cooper <nac33@students.uwf.edu>
2018-04-21 23:31:57 -0500

    Added functionality to have user returned to main screen when they have successfully submitted the form and the post request is finished

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -22,7 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -46,6 +51,7 @@ class PostsNew extends Component {
           component = { this.renderField }
         />
         <button type = "submit" className = "btn btn-primary">Submit</button>
+        <Link className = "btn btn-danger" to = "/">Cancel</Link>
       </form>
     );
   }
@@ -75,4 +81,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
